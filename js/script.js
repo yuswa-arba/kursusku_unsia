710c9e511050a69ed36031abc9bd08741ec19870
yuswa-arba <yuswa.bitcoin98@gmail.com>
2021-08-01 06:53:03 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+;(function ($) {
+
+    'use strict';
+
+    $('.testimonial-wrap').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+
+    $('.portfolio-gallery').each(function () {
+        $(this).find('.popup-gallery').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+    });
+
+})(jQuery);
